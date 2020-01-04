3ddf3da8177544ebd1bd5995cf886148a921cee8
ftp <elwahab_grodd@bora.beget.ru>
2020-01-04 18:27:29 +0300

    work package

---
+++ src/Telegram.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ELWAHAB;
+
+class Telegram
+{
+    public function __construct()
+    {
+        echo "My first library. WWWWW";
+        // echo __DIR__ .'/'. __FILE__ .'/'. __CLASS__;
+    }
+}
