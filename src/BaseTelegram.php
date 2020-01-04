deeef4629237c0ca216847762f8a9a93e1d72d73
ftp <elwahab_grodd@bora.beget.ru>
2020-01-04 20:11:34 +0300

    all work, add methods

---
+++ src/BaseTelegram.php
@@ -0,0 +1,71 @@
+<?
+
+namespace ELWAHAB;
+
+class BaseTelegram
+{
+    protected $token;
+    protected $base_url = 'https://api.telegram.org/bot';
+
+    /**
+     * The function sends any method to the telegram for processing
+     *
+     * @param    typeMethod string
+     * @param    params array with params for Telegram API
+     * @return    result json result from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    protected function methodType($typeMethod = 'sendMessage', $params)
+    {
+        $baseURL = $this->base_url . $this->token .'/';
+        $baseURL .= $typeMethod;
+
+        $url = $baseURL. '?'. http_build_query($params);
+
+        $result = $this->curlQuery($url);
+        return $result;
+    }
+
+    /**
+     * Function for get json, and other
+     *
+     * @param     url string
+     * @param     header array with headers
+     * @param     fields array with post fields
+     * @param     typePOST string, type send post
+     * @param     cookieFile string, cookie file with site
+     * @return    content string
+     * @author    Andrii_Unhurian
+     */
+    protected function curlQuery($url, $header = false, $fields = null, $typePOST = 'http', $cookieFile = 'cookie.txt'){
+    	$curl = curl_init();
+
+    	$option = [
+    			CURLOPT_URL => $url,
+    			CURLOPT_HEADER => $header,
+    			CURLOPT_RETURNTRANSFER => true,
+    			CURLOPT_FOLLOWLOCATION => true
+    			];
+
+    	if(!empty($fields)){
+            if (strpos($typePOST, 'http') !== false) {
+                $option[CURLOPT_POSTFIELDS] = http_build_query($fields);
+            }elseif (strpos($typePOST, 'json') !== false) {
+                $option[CURLOPT_POSTFIELDS] = json_encode($fields);
+            }else{
+                $option[CURLOPT_POSTFIELDS] = $fields;
+            }
+    	}
+
+    	$option[CURLOPT_COOKIEFILE] = $cookieFile;
+    	$option[CURLOPT_COOKIEJAR] = $cookieFile;
+
+    	curl_setopt_array($curl, $option);
+
+    	$content = curl_exec($curl);
+    	curl_close($curl);
+
+    	return $content;
+    }
+
+}
--- src/Telegram.php
+++ src/Telegram.php
@@ -2,11 +2,193 @@
 
 namespace ELWAHAB;
 
-class Telegram
+use ELWAHAB\BaseTelegram;
+
+class Telegram extends BaseTelegram
 {
-    public function __construct()
+
+    public function __construct($token)
     {
-        echo "My first library. WWWWW";
-        // echo __DIR__ .'/'. __FILE__ .'/'. __CLASS__;
+        $this->token = $token;
     }
+
+    /**
+     * Function for sending telegram messages by bot
+     *
+     * @param     chat_id number | id user what get message
+     * @param     text string | message to chat_id
+     * @param     markdown string | markdown or html
+     * @return    result json from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    public function sendMessage($chat_id, $text, $markdown = null)
+    {
+        $typeMethod = 'sendMessage';
+
+        $params = [
+            'chat_id'   => $chat_id,
+            'text'      => $text
+        ];
+
+        if (!empty($markdown)) {
+            if (strpos($markdown, 'markdown') !== false) {
+                $params['parse_mode'] = 'Markdown';
+            }elseif (strpos($markdown, 'html') !== false) {
+                $params['parse_mode'] = 'HTML';
+            }
+        }
+
+        $result = $this->methodType($typeMethod, $params);
+
+        return  $result;
+    }
+
+    /**
+     * For forwardMessage from one chat to other chat
+     *
+     * @param     chat_id integer | id chat where you want send message
+     * @param     from_chat_id integer | id chat where you get message to send
+     * @param     message_id integer | id message what you want send
+     * @return    result json from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    public function forwardMessage($chat_id, $from_chat_id, $message_id)
+    {
+        $typeMethod = 'forwardMessage';
+
+        $params = [
+            'chat_id'       => $chat_id,
+            'from_chat_id'  => $from_chat_id,
+            'message_id'    => $message_id
+        ];
+
+        $result = $this->methodType($typeMethod, $params);
+
+        return  $result;
+    }
+
+    /**
+     * Method for delete message from chat
+     *
+     * @param     chat_id integer | id chat where you want delete message
+     * @param     message_id integer | id message what you want delete
+     * @return    result json from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    public function deleteMessage($chat_id, $message_id)
+    {
+        $typeMethod = 'deleteMessage';
+
+        $params = [
+            'chat_id'       => $chat_id,
+            'message_id'    => $message_id
+        ];
+
+        $result = $this->methodType($typeMethod, $params);
+
+        return  $result;
+    }
+
+    /**
+     * Method for send photo in chat
+     *
+     * @param     chat_id integer | id chat where you want send photo
+     * @param     path string | path to photo what you want send
+     * @return    result json from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    public function sendPhoto($chat_id, $path)
+    {
+        $typeMethod = 'sendPhoto';
+
+        $params = [
+            'chat_id'   => $chat_id,
+            'photo'     => $path,
+        ];
+
+        $result = $this->methodType($typeMethod, $params);
+
+        return  $result;
+    }
+
+    /**
+     * Method for send video in chat // TODO: method don`t work now
+     *
+     * @param     chat_id integer | id chat where you want send video
+     * @param     path string | path to video what you want send
+     * @return    result json from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    public function sendVideo($chat_id, $path)
+    {
+        $typeMethod = 'sendVideo';
+
+        $params = [
+            'chat_id'  => $chat_id,
+            'video'    => $path,
+        ];
+
+        $result = $this->methodType($typeMethod, $params);
+
+        return  $result;
+    }
+
+    /**
+     * Function for sending telegram messages by bot
+     *  customer params send
+     *
+     * @param     params array | array with field to send message
+     * @return    result json from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    public function sendMessageParams($params)
+    {
+        $typeMethod = 'sendMessage';
+
+        $result = $this->methodType($typeMethod, $params);
+
+        return  $result;
+    }
+
+    /**
+     * Function for answer callback query
+     *
+     * @param     params array | array with field to send callback
+     *  Params can be this array:
+     *
+     *  $params = [
+     *     'callback_query_id'     => $callback_id, // id callback for answer
+     *      'text'                  => $answer // text answer
+     *   ];
+     *
+     * @return    result json from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    public function answerCallbackQuery($params)
+    {
+        $typeMethod = 'answerCallbackQuery';
+
+        $result = $this->methodType($typeMethod, $params);
+
+        return $result;
+    }
+
+    /**
+     * Function for answer callback query || maybe, no effective
+     *
+     * @param     params array | array with field to send callback for inline click
+     * @return    result json from Telegram API
+     * @author    Andrii_Unhurian
+     */
+    public function answerInlineQuery($params)
+    {
+        $typeMethod = 'answerInlineQuery';
+
+        $result = $this->methodType($typeMethod, $params);
+
+        return $result;
+    }
+
+
+
 }
